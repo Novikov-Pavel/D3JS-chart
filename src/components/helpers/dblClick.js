45974e5ef0b414aca56e29c0b5715cf7bb42d48e
Pavel Novikov <p.novikov@mkskom.ru>
2024-01-30 17:10:11 +0300

    пофиксил функцию dblClick

--- src/components/helpers/dblClick.js
+++ src/components/helpers/dblClick.js
@@ -6,8 +6,6 @@ import config from "../config";
 export function dblclick() {
   groupDateAmount.value = d3.group(config.data, (d) => d.dateAmount);
   const newDateAmount = computed(() => groupDateAmount.value.get("amount"));
-  d3.select(".xAxis").call(d3.axisBottom(x));
-  d3.select(".brush").call(brush);
 
   if (config.typeChart === "Line") {
     x.domain(d3.extent(newDateAmount.value, (d) => parseTime(d.date)));
@@ -16,5 +14,7 @@ export function dblclick() {
     x.domain(newDateAmount.value.map((d) => d.date));
     x.padding(0.3);
   }
-
+  
+  d3.select(".xAxis").call(d3.axisBottom(x));
+  d3.select(".brush").call(brush);
 }
