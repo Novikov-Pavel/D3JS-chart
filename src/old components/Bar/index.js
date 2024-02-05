c4a343b62f7b2e1b8b02de6ae7b2f3be8c14c4b2
Pavel Novikov <p.novikov@mkskom.ru>
2024-02-05 13:10:49 +0300

    deleted old components

--- src/old components/Bar/index.js
+++
@@ -1,2 +0,0 @@
-export { default as BarChartHoriz } from "./BarChartHoriz.vue";
-export { default as BarChartVertic } from "./BarChartVertic.vue";
--- src/old components/index.js
+++
@@ -1,3 +0,0 @@
-// export { default as Chart } from "./Chart";
-// export { default as LineWithTooltip } from "./LineWithTooltip";
-// export { BarChartHoriz, BarChartVertic } from "./Bar";
